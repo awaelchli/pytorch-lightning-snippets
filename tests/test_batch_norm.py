eb2f0ef2f25f7f3f89cbb6d97e35fe5713680e3b
Adrian Wälchli <adrian.waelchli@inf.unibe.ch>
2020-08-24 02:59:04 +0200

    refactor the callback

--- tests/test_batch_norm.py
+++ tests/test_batch_norm.py
@@ -2,7 +2,10 @@ import pytest
 import torch
 from torch import nn as nn
 
-from pytorch_lightning import Trainer
+from pytorch_lightning import Trainer, LightningModule, TrainResult
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+
 from verification.batch_norm import BatchNormVerification, BatchNormVerificationCallback
 
 
@@ -12,11 +15,33 @@ class ConvBiasBatchNormModel(nn.Module):
         super().__init__()
         self.conv = nn.Conv2d(3, 5, kernel_size=1, bias=use_bias)
         self.bn = nn.BatchNorm2d(5)
-        self.example_input_array = torch.rand(2, 3, 10, 10)
+        self.input_array = torch.rand(2, 3, 10, 10)
 
     def forward(self, x):
         # x: (B, 3, H, W)
         return self.bn(self.conv(x))
+
+
+class LitModel(LightningModule):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.model = ConvBiasBatchNormModel(*args, **kwargs)
+        self.example_input_array = self.model.input_array
+
+    def forward(self, *args, **kwargs):
+        return self.model(*args, **kwargs)
+
+    def train_dataloader(self):
+        return DataLoader(TensorDataset(torch.rand(32, 3, 10, 10)), batch_size=4)
+
+    def configure_optimizers(self):
+        return Adam(self.parameters())
+
+    def training_step(self, batch, batch_idx):
+        output = self(batch[0])
+        loss = output.sum()
+        return TrainResult(minimize=loss)
 
 
 @pytest.mark.parametrize(["use_bias"], [
@@ -27,19 +52,20 @@ def test_conv_bias_batch_norm_model(use_bias):
     model = ConvBiasBatchNormModel(use_bias=use_bias)
     verification = BatchNormVerification(model)
     expected = not use_bias
-    result = verification.check(input_array=model.example_input_array)
+    result = verification.check(input_array=model.input_array)
     assert result == expected
 
 
 def test_conv_bias_batch_norm_callback():
-    trainer = Trainer()
-    model = ConvBiasBatchNormModel(use_bias=True)
-    expected = "'conv' with bias followed by a normalization layer 'bn'"
+    model = LitModel(use_bias=True)
+    expected = "'model.conv' with bias followed by a normalization layer 'model.bn'"
 
     callback = BatchNormVerificationCallback()
+    trainer = Trainer(callbacks=[callback], max_steps=1)
     with pytest.warns(UserWarning, match=expected):
-        callback.on_train_start(trainer, model)
+        trainer.fit(model)
 
     callback = BatchNormVerificationCallback(error=True)
+    trainer = Trainer(callbacks=[callback], max_steps=1)
     with pytest.raises(RuntimeError, match=expected):
-        callback.on_train_start(trainer, model)
+        trainer.fit(model)
--- verification/batch_norm.py
+++ verification/batch_norm.py
@@ -21,8 +21,9 @@ class BatchNormVerification(VerificationBase):
 
     def __init__(self, model: nn.Module):
         super().__init__(model)
-        self._detected_pairs = []
+        self._hook_handles = []
         self._module_sequence = []
+        self._detected_pairs = []
 
     @property
     def detected_pairs(self) -> List[Tuple]:
@@ -30,15 +31,15 @@ class BatchNormVerification(VerificationBase):
 
     def check(self, input_array=None) -> bool:
         input_array = self._get_input_array_copy(input_array)
-        hook_handles = self._register_hooks()
-
+        self.register_hooks()
         # trigger the hooks and collect sequence of layers
         self._model_forward(input_array)
-
-        for hook in hook_handles:
-            hook.remove()
-
-        self._detected_pairs = []
+        self.destroy_hooks()
+        self.collect_detections()
+        return not self._detected_pairs
+    
+    def collect_detections(self):
+        detected_pairs = []
         for (name0, mod0), (name1, mod1) in zip(self._module_sequence[:-1], self._module_sequence[1:]):
             bias = getattr(mod0, "bias", None)
             detected = (
@@ -48,42 +49,52 @@ class BatchNormVerification(VerificationBase):
                 and bias.requires_grad
             )
             if detected:
-                self._detected_pairs.append((name0, name1))
+                detected_pairs.append((name0, name1))
+        self._detected_pairs = detected_pairs
+        return detected_pairs
 
-        return not self._detected_pairs
+    def register_hooks(self):
+        hook_handles = []
+        for name, module in self.model.named_modules():
+            handle = module.register_forward_hook(self._create_hook(name))
+            hook_handles.append(handle)
+        self._hook_handles = hook_handles
 
     def _create_hook(self, module_name) -> Callable:
         def hook(module, inp_, out_):
             self._module_sequence.append((module_name, module))
         return hook
 
-    def _register_hooks(self) -> list:
-        hook_handles = []
-        for name, module in self.model.named_children():
-            handle = module.register_forward_hook(self._create_hook(name))
-            hook_handles.append(handle)
-        return hook_handles
+    def destroy_hooks(self):
+        for hook in self._hook_handles:
+            hook.remove()
+        self._hook_handles = []
 
 
 class BatchNormVerificationCallback(VerificationCallbackBase):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self._verification = None
 
     def message(self, detections: List[Tuple]) -> str:
         first_detection = detections[0]
         message = (
             f"Detected a layer '{first_detection[0]}' with bias followed by"
             f" a normalization layer '{first_detection[1]}'."
-            f" This makes the normalization ineffective and can lead to unstable training. "
+            f" This makes the normalization ineffective and can lead to unstable training."
             f" Either remove the normalization or turn off the bias."
         )
         return message
 
     def on_train_start(self, trainer, pl_module):
-        verification = BatchNormVerification(pl_module)
-        result = verification.check(
-            input_array=pl_module.example_input_array,
-        )
-        if not result:
-            self._raise(verification.detected_pairs)
+        self._verification = BatchNormVerification(pl_module)
+        self._verification.register_hooks()
+
+    def on_train_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
+        if batch_idx > 0:
+            return
+        detected_pairs = self._verification.collect_detections()
+        if detected_pairs:
+            self._raise(detections=detected_pairs)
+        self._verification.destroy_hooks()
