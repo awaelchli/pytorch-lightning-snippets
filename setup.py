0c4a81e056f47936bde65144ecc0f980f00f9b4d
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-09-02 06:16:35 +0200

    setup script

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pytorch-lightning-snippets',
+    version='0.0.1',
+    author='Adrian Wälchli',
+    author_email='aedu.waelchli@gmail.com',
+    scripts=[],
+    url='https://github.com/awaelchli/pytorch-lightning-snippets.git',
+    license='LICENSE.md',
+    description='A collection of useful tools around PyTorch Lightning.',
+    long_description=open('README.md').read(),
+    packages=find_packages(exclude=['tests', 'examples']),
+    install_requires=[
+        'pytorch-lightning>=0.9.0',
+    ],
+)
