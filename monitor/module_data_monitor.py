5362a3048965a8be6824502ae4c8ba93065ab06a
Adrian Wälchli <aedu.waelchli@gmail.com>
2020-09-07 07:26:18 +0200

    improved submodule logging

--- monitor/module_data_monitor.py
+++ monitor/module_data_monitor.py
@@ -1,40 +1,85 @@
-from typing import List, Optional
+from typing import List, Optional, Union
+
+import torch.nn as nn
 
 from monitor.data_monitor_base import DataMonitorBase
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.utilities import rank_zero_warn
-import torch.nn as nn
 
 
 class ModuleDataMonitor(DataMonitorBase):
 
-    def __init__(self, submodules: Optional[List[str]] = None, row_log_interval: int = None):
+    def __init__(self, submodules: Optional[Union[bool, List[str]]] = None, row_log_interval: int = None):
+        """
+        Args:
+            submodules: If `True`, logs the in- and output histograms of every submodule in the
+                LightningModule, including the root module itself.
+                This parameter can also take a list of names of specifc submodules (see example below).
+                Default: `None`, logs only the in- and output of the root module.
+            row_log_interval: The interval at which histograms should be logged. This defaults to the
+                interval defined in the Trainer. Use this to override the Trainer default.
+
+        Note:
+            A too low value for `row_log_interval` may have a significant performance impact
+            especially when many submodules are involved, since the logging occurs during the forward pass.
+            It should only be used for debugging purposes.
+
+        Example:
+
+            .. code-block:: python
+
+                # log the in- and output histograms of the `forward` in LightningModule
+                trainer = Trainer(callbacks=[ModuleDataMonitor()])
+
+                # all submodules in LightningModule
+                trainer = Trainer(callbacks=[ModuleDataMonitor(submodules=True)])
+
+                # specific submodules
+                trainer = Trainer(callbacks=[ModuleDataMonitor(submodules=["generator", "generator.conv1"])])
+
+        """
         super().__init__(row_log_interval=row_log_interval)
         self._submodule_names = submodules
         self._hook_handles = []
 
+    def submodule_names(self, root_module: nn.Module):
+        # default is the root module only
+        names = [""]
+
+        if isinstance(self._submodule_names, list):
+            names = self._submodule_names
+
+        if self._submodule_names is True:
+            names = [name for name, _ in root_module.named_modules()]
+
+        return names
+
     def on_train_start(self, trainer: Trainer, pl_module: LightningModule):
         super().on_train_start(trainer, pl_module)
         submodule_dict = dict(pl_module.named_modules())
-
-        if self._submodule_names is None:
-            handle = self.register_hook("root", pl_module)
-            self._hook_handles = [handle]
-        else:
-            self._hook_handles = []
-            for name in self._submodule_names:
-                handle = self.register_hook(name, submodule_dict[name])
-                self._hook_handles.append(handle)
+        self._hook_handles = []
+        for name in self.submodule_names(pl_module):
+            if name not in submodule_dict:
+                rank_zero_warn(
+                    f"{name} is not a valid identifier for a submodule in {pl_module.__class__.__name__},"
+                    " skipping this key."
+                )
+                continue
+            handle = self.register_hook(name, submodule_dict[name])
+            self._hook_handles.append(handle)
 
     def on_train_end(self, trainer, pl_module):
         for handle in self._hook_handles:
             handle.remove()
 
     def register_hook(self, module_name: str, module: nn.Module):
+        input_group_name = f"input/{module_name}" if module_name else "input"
+        output_group_name = f"output/{module_name}" if module_name else "output"
 
         def hook(_, inp, out):
-            self.log_histograms(inp, group=f"input/{module_name}")
-            self.log_histograms(out, group=f"output/{module_name}")
+            inp = inp[0] if len(inp) == 1 else inp
+            self.log_histograms(inp, group=input_group_name)
+            self.log_histograms(out, group=output_group_name)
 
         handle = module.register_forward_hook(hook)
         return handle
